b83ffc95d4019b44e19bf1af83c5e7fee714deb1
Adam Oxner <boxner@gmail.com>
2013-11-03 16:00:33 -0500

    Initial Commit

---
+++ OpnSwim2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpnSwim2
+//
+//  Created by Adam Oxner on 11/3/13.
+//  Copyright (c) 2013 University of Michigan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
